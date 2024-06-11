0c2fb6575d4dc0c08e021182ca77d1f99e91cdd0
Kirill Kozlov <space_cell@proton.me>
2024-06-11 16:51:45 +0500

    Init commit

---
+++ src/main/java/com/example/spring_db_demo/SpringDbDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.spring_db_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDbDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDbDemoApplication.class, args);
+	}
+
+}
